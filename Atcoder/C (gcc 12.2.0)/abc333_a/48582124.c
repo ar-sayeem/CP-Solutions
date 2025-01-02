86841474292b59469e49d6dbf47a7345dbe1e86d
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2025-01-02 20:59:55 +0600

    Committing files

---
+++ Atcoder/C (gcc 12.2.0)/abc333_a/48582124.c
@@ -0,0 +1,17 @@
+/// AtCoder Beginner Contest 333
+/// A - Three Threes
+
+#include<stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for(int i=0; i<N; i++)
+    {
+        printf("%d", N);
+    }
+
+    return 0;
+}
+
