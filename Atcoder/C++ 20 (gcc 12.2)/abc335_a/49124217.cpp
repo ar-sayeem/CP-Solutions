7e3b8967c5c9bae419c4b1f9dbd47b87c8a5f477
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2025-01-02 20:59:51 +0600

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc335_a/49124217.cpp
@@ -0,0 +1,15 @@
+/// AtCoder Beginner Contest 335 (Sponsored by Mynavi)
+/// 06 January, 2023
+/// A	202<s>3</s>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    s[s.length()-1] = '4';
+    cout << s;
+    return 0;
+}
